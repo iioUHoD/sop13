1c0a10789033d8eacb41a9182c73306e771ee3cf
iioUHoD <41914088+iioUHoD@users.noreply.github.com>
2022-11-14 17:28:35 +0700

    Add files via upload

---
+++ bookstore-service/src/test/java/com/sop/bookstoreservice/BookstoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sop.bookstoreservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
